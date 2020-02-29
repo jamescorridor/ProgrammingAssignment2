8828ffb59eca9e07a4304883d1923ceafb82221e
jamescorridor <61564233+jamescorridor@users.noreply.github.com>
2020-02-29 11:15:34 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -25,7 +25,7 @@ cacheSolve <- function(x, ...) {
   if(!is.null(inv)) {
     message("getting cached result")
     return(inv)
-  }
+  } 
   data <- x$get()
   inv <- solve(data, ...)
   x$setinv(inv)
